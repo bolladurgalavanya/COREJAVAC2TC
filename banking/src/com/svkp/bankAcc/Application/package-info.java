3eefff8abcc9f66b1d2b42f292e10fb11d582411
lavan <lavan@lavanya.bbrouter>
2022-08-25 10:22:40 +0530

    Banking

---
+++ banking/src/com/svkp/bankAcc/Application/package-info.java
@@ -0,0 +1,1 @@
+package com.svkp.bankAcc.Application;
---
+++ banking/src/com/svkp/bankAcc/Framework/CurrentAcc.java
@@ -0,0 +1,14 @@
+package com.svkp.bankAcc.Framework;
+
+public class CurrentAcc {
+    public final float accountbalance=0;
+    
+	public CurrentAcc(int accno, String accname, float accBal, boolean deposit) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+
+}
